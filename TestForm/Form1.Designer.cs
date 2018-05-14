d11cb0bc8f5fe0f624a9df535a2f1d1f5001b2d9
HaoNguyen <hoanghao520@gmail.com>
2018-05-14 15:22:20 +0700

    Upload source

---
+++ TestForm/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿using System;
+
+namespace MainForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("C");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("D");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Google Drive");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Dropbox");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("One Drive");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Cloud", new System.Windows.Forms.TreeNode[] {
+            treeNode3,
+            treeNode4,
+            treeNode5});
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItemCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemPaste = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemProperties = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.listView = new System.Windows.Forms.ListView();
+            this.imageListIcon = new System.Windows.Forms.ImageList(this.components);
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView
+            // 
+            this.treeView.ContextMenuStrip = this.contextMenuStrip;
+            this.treeView.ImageKey = "open.png";
+            this.treeView.ImageList = this.imageList;
+            this.treeView.Location = new System.Drawing.Point(-1, -1);
+            this.treeView.Name = "treeView";
+            treeNode1.Name = "NodeC";
+            treeNode1.Text = "C";
+            treeNode2.Name = "NodeD";
+            treeNode2.Text = "D";
+            treeNode3.Name = "NodeGGD";
+            treeNode3.Text = "Google Drive";
+            treeNode4.Name = "NodeDB";
+            treeNode4.Text = "Dropbox";
+            treeNode5.Name = "NodeOD";
+            treeNode5.Text = "One Drive";
+            treeNode6.Name = "NodeCloud";
+            treeNode6.Text = "Cloud";
+            this.treeView.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode6});
+            this.treeView.SelectedImageIndex = 0;
+            this.treeView.Size = new System.Drawing.Size(260, 389);
+            this.treeView.TabIndex = 0;
+            this.treeView.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView_NodeMouseClick);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemCopy,
+            this.toolStripMenuItemPaste,
+            this.toolStripMenuItemDelete,
+            this.toolStripMenuItemProperties});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(128, 92);
+            // 
+            // toolStripMenuItemCopy
+            // 
+            this.toolStripMenuItemCopy.Name = "toolStripMenuItemCopy";
+            this.toolStripMenuItemCopy.Size = new System.Drawing.Size(127, 22);
+            this.toolStripMenuItemCopy.Text = "Copy";
+            this.toolStripMenuItemCopy.Click += new System.EventHandler(this.toolStripMenuItemCopy_Click);
+            // 
+            // toolStripMenuItemPaste
+            // 
+            this.toolStripMenuItemPaste.Name = "toolStripMenuItemPaste";
+            this.toolStripMenuItemPaste.Size = new System.Drawing.Size(127, 22);
+            this.toolStripMenuItemPaste.Text = "Paste";
+            this.toolStripMenuItemPaste.Click += new System.EventHandler(this.toolStripMenuItemPaste_Click);
+            // 
+            // toolStripMenuItemDelete
+            // 
+            this.toolStripMenuItemDelete.Name = "toolStripMenuItemDelete";
+            this.toolStripMenuItemDelete.Size = new System.Drawing.Size(127, 22);
+            this.toolStripMenuItemDelete.Text = "Delete";
+            this.toolStripMenuItemDelete.Click += new System.EventHandler(this.toolStripMenuItemDelete_Click);
+            // 
+            // toolStripMenuItemProperties
+            // 
+            this.toolStripMenuItemProperties.Name = "toolStripMenuItemProperties";
+            this.toolStripMenuItemProperties.Size = new System.Drawing.Size(127, 22);
+            this.toolStripMenuItemProperties.Text = "Properties";
+            this.toolStripMenuItemProperties.Click += new System.EventHandler(this.toolStripMenuItemProperties_Click);
+            // 
+            // imageList
+            // 
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "new-folder.png");
+            this.imageList.Images.SetKeyName(1, "new-file.png");
+            this.imageList.Images.SetKeyName(2, "xml-file.png");
+            this.imageList.Images.SetKeyName(3, "txt-file.png");
+            // 
+            // listView
+            // 
+            this.listView.Location = new System.Drawing.Point(253, -1);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(443, 389);
+            this.listView.SmallImageList = this.imageListIcon;
+            this.listView.StateImageList = this.imageListIcon;
+            this.listView.TabIndex = 1;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // imageListIcon
+            // 
+            this.imageListIcon.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListIcon.ImageStream")));
+            this.imageListIcon.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListIcon.Images.SetKeyName(0, "disk.png");
+            this.imageListIcon.Images.SetKeyName(1, "folder__plus.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 387);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.treeView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        private void treeView_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemCopy;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemPaste;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemDelete;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemProperties;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ImageList imageListIcon;
+    }
+}
+
---
+++ TestForm/Form1.cs
@@ -0,0 +1,419 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MainForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public List<string> listFile = new List<string>();
+
+        DirectoryInfo directoryInfoC = new DirectoryInfo(@"C:\");
+        DirectoryInfo directoryInfoD = new DirectoryInfo(@"D:\");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadDirectories(directoryInfoC, treeView.Nodes[0]);
+            loadDirectories(directoryInfoD, treeView.Nodes[1]);
+            //if (Directory.Exists(@"C:\"))
+            //{
+            //    try
+            //    {
+            //        DirectoryInfo[] directories = directoryInfoC.GetDirectories();
+
+            //        foreach (FileInfo file in directoryInfoC.GetFiles())
+            //        {
+            //            if (file.Exists)
+            //            {
+            //                TreeNode nodes = treeView.Nodes[0].Nodes.Add(file.Name);
+            //                SetImageExtension(file.Name, nodes);
+            //            }
+            //        }
+
+            //        if (directories.Length > 0)
+            //        {
+            //            foreach (DirectoryInfo directory in directories)
+            //            {
+            //                TreeNode node = treeView.Nodes[0].Nodes.Add(directory.Name);
+            //                node.ImageIndex = node.SelectedImageIndex = 0;
+            //                foreach (FileInfo file in directory.GetFiles())
+            //                {
+            //                    if (file.Exists)
+            //                    {
+            //                        TreeNode nodes = treeView.Nodes[0].Nodes[node.Index].Nodes.Add(file.Name);
+            //                        SetImageExtension(file.Name, nodes);
+            //                    }
+            //                }
+            //            }
+            //        }
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        MessageBox.Show(ex.Message);
+            //    }
+            //}
+            //if (Directory.Exists(@"D:\"))
+            //{
+            //    try
+            //    {
+            //        DirectoryInfo[] directories = directoryInfoD.GetDirectories();
+
+            //        foreach (FileInfo file in directoryInfoD.GetFiles())
+            //        {
+            //            if (file.Exists)
+            //            {
+            //                TreeNode nodes = treeView.Nodes[1].Nodes.Add(file.Name);
+            //                SetImageExtension(file.Name, nodes);
+            //            }
+            //        }
+
+            //        if (directories.Length > 0)
+            //        {
+            //            foreach (DirectoryInfo directory in directories)
+            //            {
+            //                TreeNode node = treeView.Nodes[1].Nodes.Add(directory.Name);
+            //                node.ImageIndex = node.SelectedImageIndex = 0;
+            //                foreach (FileInfo file in directory.GetFiles())
+            //                {
+            //                    if (file.Exists)
+            //                    {
+            //                        TreeNode nodes = treeView.Nodes[1].Nodes[node.Index].Nodes.Add(file.Name);
+            //                        SetImageExtension(file.Name, nodes);
+            //                    }
+            //                }
+            //            }
+            //        }
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        MessageBox.Show(ex.Message);
+            //    }
+            //}
+
+            //listViewLoad("");
+        }
+
+        private void toolStripMenuItemCopy_Click(object sender, EventArgs e)
+        {
+            if (treeView.SelectedNode != null)
+            {
+                try
+                {
+                    DirectoryInfo[] directories = directoryInfoC.GetDirectories();
+                    foreach (FileInfo file in directoryInfoC.GetFiles())
+                    {
+                        if (file.Exists && file.Name == treeView.SelectedNode.Text)
+                        {
+                            StringCollection filePath = new StringCollection();
+                            filePath.Add(file.FullName);
+                            Clipboard.SetFileDropList(filePath);
+                        }
+                    }
+
+                    if (directories.Length > 0)
+                    {
+                        foreach (DirectoryInfo directory in directories)
+                        {
+                            if (directory.Name == treeView.SelectedNode.Text)
+                            {
+                                StringCollection folderPath = new StringCollection();
+                                folderPath.Add(directory.FullName);
+                                Clipboard.SetFileDropList(folderPath);
+                            }
+
+                            foreach (FileInfo file in directory.GetFiles())
+                            {
+                                if (file.Exists && file.Name == treeView.SelectedNode.Text)
+                                {
+                                    StringCollection filePath = new StringCollection();
+                                    filePath.Add(file.FullName);
+                                    Clipboard.SetFileDropList(filePath);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void toolStripMenuItemPaste_Click(object sender, EventArgs e)
+        {
+            if (treeView.SelectedNode != null)
+            {
+                bool copy = false;
+                try
+                {
+                    DirectoryInfo[] directories = directoryInfoC.GetDirectories();
+                    if (directories.Length > 0)
+                    {
+                        foreach (DirectoryInfo directory in directories)
+                        {
+                            if (directory.Name == treeView.SelectedNode.Text && Clipboard.ContainsFileDropList())
+                            {
+                                foreach (string file in Clipboard.GetFileDropList())
+                                {
+                                    string targetDir = directoryInfoC.FullName + @"\" + directory.Name;
+                                    File.Copy(Path.Combine(file.Replace(Path.GetFileName(file), ""), Path.GetFileName(file)), Path.Combine(targetDir, Path.GetFileName(file)), true);
+                                }
+                                copy = true;
+                            }
+                        }
+                    }
+
+                    if (copy)
+                    {
+                        foreach (string file in Clipboard.GetFileDropList())
+                        {
+                            TreeNode node = treeView.Nodes[0].Nodes[treeView.SelectedNode.Index].Nodes.Add(Path.GetFileName(file));
+                            SetImageExtension(file, node);
+                        }
+                        copy = false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    copy = false;
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void toolStripMenuItemDelete_Click(object sender, EventArgs e)
+        {
+            if (treeView.SelectedNode != null)
+            {
+                bool deleted = false;
+                try
+                {
+                    DirectoryInfo[] directories = directoryInfoC.GetDirectories();
+                    foreach (FileInfo file in directoryInfoC.GetFiles())
+                    {
+                        if (file.Exists && file.Name == treeView.SelectedNode.Text)
+                        {
+                            file.Delete();
+                            deleted = true;
+                        }
+                    }
+
+                    if (directories.Length > 0)
+                    {
+                        foreach (DirectoryInfo directory in directories)
+                        {
+                            foreach (FileInfo file in directory.GetFiles())
+                            {
+                                if (file.Exists && file.Name == treeView.SelectedNode.Text)
+                                {
+                                    file.Delete();
+                                    deleted = true;
+                                }
+                            }
+
+                            if (treeView.SelectedNode.Text == directory.Name)
+                            {
+                                foreach (FileInfo file in directory.GetFiles())
+                                {
+                                    if (file.Exists)
+                                        file.Delete();
+                                }
+                                directory.Delete();
+                                deleted = true;
+                            }
+                        }
+                    }
+
+                    if (deleted)
+                        treeView.SelectedNode.Remove();
+                }
+                catch (Exception ex)
+                {
+                    deleted = false;
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        ExplorerProperties properties;
+        private void toolStripMenuItemProperties_Click(object sender, EventArgs e)
+        {
+            if (treeView.SelectedNode != null)
+            {
+                if (properties != null)
+                    properties.Close();
+
+                DirectoryInfo[] directories = directoryInfoC.GetDirectories();
+
+                foreach (DirectoryInfo directory in directories)
+                {
+                    if (!treeView.SelectedNode.Text.Contains(".")) // Folder, not a file
+                    {
+                        if (directory.Name == treeView.SelectedNode.Text)
+                        {
+                            properties = new ExplorerProperties(directory.Name, directory.FullName, CalculateBytes(directory.GetFiles("*.*", SearchOption.AllDirectories).Sum(file => file.Length)) + " (" + directory.GetFiles("*.*", SearchOption.AllDirectories).Sum(file => file.Length).ToString() + " bytes)");
+                            properties.Show();
+                        }
+                    }
+                    else
+                    {
+                        foreach (FileInfo file in directoryInfoC.GetFiles())
+                        {
+                            if (file.Name == treeView.SelectedNode.Text)
+                            {
+                                properties = new ExplorerProperties(file.Name, file.FullName, CalculateBytes(file.Length) + " (" + file.Length + " bytes)");
+                                properties.Show();
+                                return;
+                            }
+                        }
+
+                        foreach (FileInfo file in directory.GetFiles())
+                        {
+                            if (file.Name == treeView.SelectedNode.Text)
+                            {
+                                properties = new ExplorerProperties(file.Name, file.FullName, CalculateBytes(file.Length) + " (" + file.Length + " bytes)");
+                                properties.Show();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        #region Calculation
+        private string CalculateBytes(long count)
+        {
+            string[] sizeNames = { " B", " KB", " MB", " GB", " TB", " PB", " EB" };
+            if (count == 0)
+                return "0" + sizeNames[0];
+            long bytes = Math.Abs(count);
+            int log = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            double number = Math.Round(bytes / Math.Pow(1024, log), 1);
+            return (Math.Sign(count) * number).ToString() + sizeNames[log];
+        }
+        #endregion
+
+        #region Extensions
+        private void SetImageExtension(string name, TreeNode _node)
+        {
+            if (name.Contains("xml"))
+                _node.ImageIndex = _node.SelectedImageIndex = 2;
+            else if (name.Contains("txt"))
+                _node.ImageIndex = _node.SelectedImageIndex = 3;
+        }
+        #endregion
+
+        private void treeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            listViewLoad(treeView_GetPathOfNode(e.Node));
+
+        }
+
+        private string treeView_GetPathOfNode(TreeNode node)
+        {
+            if (node.Text != "C" && node.Text != "D")
+            {
+                return treeView_GetPathOfNode(node.Parent) + @"\" + node.Text;
+            }
+            else
+            {
+                return node.Text + @":\";
+            }
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void listViewLoad(string Path)
+        {
+            //Path = @"D:\UIT\Storage_Data\nemiro.oauth.dll-master\examples\DropboxExample\Resources";
+            try
+            {
+                if (listView != null)
+                {
+                    listView.Items.Clear();
+                    listFile.Clear();
+                }
+                foreach (string item in Directory.GetFiles(Path))
+                {
+                    imageListIcon.Images.Add(System.Drawing.Icon.ExtractAssociatedIcon(item));
+                    FileInfo fi = new FileInfo(item);
+                    listFile.Add(fi.FullName);
+                    listView.Items.Add(fi.Name, 0);
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+
+        private void treeView_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            string path = treeView_GetPathOfNode(e.Node);
+            listViewLoad(path);
+            loadDirectories(new DirectoryInfo(path), e.Node);
+        }
+
+        private void loadDirectories(DirectoryInfo directoryPath, TreeNode node)
+        {
+            try
+            {
+                DirectoryInfo[] directories = directoryPath.GetDirectories();
+                if (directories.Length > 0 && node.Nodes.Count <= directories.Length)
+                {
+                    foreach (DirectoryInfo directory in directories)
+                    {
+                        //show directories
+                        node.Nodes.Add(directory.Name);
+                        treeView.SelectedNode = node;
+                        //treeView.Refresh();
+                        //loadDirectoriesAndFiles(directory, nodes);
+                    }
+                    //FileInfo[] files = directoryPath.GetFiles();
+                    //foreach (FileInfo file in files)
+                    //{
+                    //    //show file
+                    //    if (file.Exists)
+                    //    {
+                    //        TreeNode nodes = node.Nodes.Add(file.Name);
+                    //        SetImageExtension(file.Name, nodes);
+                    //    }
+                    //}
+                }
+                else
+                {
+                    return;
+                }
+            }
+            catch (System.UnauthorizedAccessException)
+            {
+                //return new TreeNode("Unavailable Node");
+                return;
+            }
+            catch (System.IO.PathTooLongException)
+            {
+                //return new TreeNode("Unavailable Node");
+                return;
+            }
+
+        }
+    }
+}
